7332229f994328543ae486ac156d509cf2071c6d
Lisa Nicvert <lisa.nicvert@univ-lyon1.fr>
2023-02-02 15:56:57 +0100

    Created R project

---
+++ R/camtrapviz-package.R
@@ -0,0 +1,26 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom shiny dataTableOutput
+#' @importFrom shiny fileInput
+#' @importFrom shiny fluidRow
+#' @importFrom shiny h2
+#' @importFrom shiny h3
+#' @importFrom shiny icon
+#' @importFrom shiny need
+#' @importFrom shiny renderDataTable
+#' @importFrom shiny req
+#' @importFrom shiny validate
+#' @importFrom shinydashboard box
+#' @importFrom shinydashboard dashboardBody
+#' @importFrom shinydashboard dashboardHeader
+#' @importFrom shinydashboard dashboardPage
+#' @importFrom shinydashboard dashboardSidebar
+#' @importFrom shinydashboard menuItem
+#' @importFrom shinydashboard menuSubItem
+#' @importFrom shinydashboard sidebarMenu
+#' @importFrom shinydashboard tabItem
+#' @importFrom shinydashboard tabItems
+## usethis namespace: end
+NULL
