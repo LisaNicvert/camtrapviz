e24dc5516393f78c5567575a0393f2e7eeb1438e
Lisa Nicvert <lisa.nicvert@univ-lyon1.fr>
2023-02-08 10:57:51 +0100

    Refactored server-side import functions

--- R/app_server.R
+++ R/app_server.R
@@ -12,11 +12,19 @@ server <- function(input, output, session) {
 # Define roots for ShinyFiles ---------------------------------------------
   roots <- c("home" = fs::path_home())
   
+# Define placeholder for optional columns ---------------------------------
+  nullval <- "Not present in data"
+  
+# Define columns for which empty is allowed -------------------------------
+  empty_allowed <- "count_col"
+  
 # Load example data -------------------------------------------------------
   utils::data(mica, package = "camtraptor")
   utils::data(recordTableSample, package = "camtrapR")
   utils::data(camtraps, package = "camtrapR")
   
+
+# Description for example datasets ----------------------------------------
   output$dyntext <- renderText({
     if(input$example_file == "mica") {
       "Muskrat and coypu camera trap observations in Belgium, the Netherlands and Germany (camtrapDP format)"
@@ -28,11 +36,14 @@ server <- function(input, output, session) {
 
 # Read files --------------------------------------------------------------
   
-  # Shiny file listener
+
+## Shiny file listener -----------------------------------------------------
   shinyFileChoose(input, 'records_input', 
                   root = c("home" = fs::path_home()), 
                   filetypes = c('csv', 'json'))
   
+
+## Get imported file extension ---------------------------------------------
   output$records_extension <- reactive({
     # Get file
     file <- shinyFiles::parseFilePaths(roots,
@@ -49,6 +60,9 @@ server <- function(input, output, session) {
   outputOptions(output, 'records_extension', 
                 suspendWhenHidden = FALSE)
   
+
+## Get dataset -------------------------------------------------------------
+
   dat <- reactive({
     if (input$input_type == 1) { # Example dataset
       if(input$example_file == "mica") {
@@ -70,7 +84,7 @@ server <- function(input, output, session) {
       # Get file extension
       ext <- tools::file_ext(file_path)
       
-      if (ext == "csv") {
+      if (ext == "csv") { # User uploaded a csv file
         # Get separator value
         sep <- input$records_sep
         
@@ -82,7 +96,7 @@ server <- function(input, output, session) {
         updateRadioButtons(inputId = "records_sep",
                            selected = res_records$sep)
         
-        if (input$import_cameras) {
+        if (input$import_cameras) { # User wants to import a camera file
           # Get file
           file <- input$cameras_input
           req(file)
@@ -99,15 +113,20 @@ server <- function(input, output, session) {
           # Update file separator
           updateRadioButtons(inputId = "cameras_sep",
                              selected = res_cameras$sep)
-        } else {
+        } else { # User doesn't want to import a camera file
           res_cameras <- NULL
         }
         
+        # Final result is a list with one component $data
+        #   This is done to match the structure of a camtrapDP file in R
+        #   The list has 2 sub-components:
+        #   $observations (records) and $deployments (cameraS)
         res <- list(data = list(observations = res_records$dat,
                                 deployments = res_cameras$dat))
-      } else if (ext == "json") {
+        
+      } else if (ext == "json") { #   CamtrapDP format
         res <- camtraptor::read_camtrap_dp(file_path, media = FALSE)
-      } else {
+      } else { # Unknown extension
         validate(need(ext == "csv" || ext == "json", 
                       "Please upload a csv file or a json datapackage"))
       }
@@ -119,14 +138,13 @@ server <- function(input, output, session) {
 
 # Columns mapping ---------------------------------------------------------
   
-  observe({
-    # Get input data columns
-    records_col <- colnames(dat()$data$observations)
-    
-    # Ensure records table exists
-    req(input$records_input)
-    
-    # Create widget list
+  # Get input data columns
+  records_cols <- reactive({
+    colnames(dat()$data$observations)
+  })
+  
+  # Get the columns we want
+  records_cols_wanted <- reactive({
     if (input$datetime_or_timestamp == 'timestamp') {
       datetime_widgets <- "timestamp_col"
     } else if (input$datetime_or_timestamp == 'date_time') {
@@ -136,39 +154,67 @@ server <- function(input, output, session) {
                      datetime_widgets,
                      "count_col")
     
-    # Columns for which no column selection is authorized
-    empty_allowed <- "count_col"
+    if (!input$import_cameras) { # User doesn't want to import a camera file
+      widget_list <- c(widget_list,
+                       "lat_col", "lon_col")
+    }
+    widget_list
+  })
+  
+  # Default columns mapping for records
+  default_records <- reactive({
+    # Ensure records table was imported and is available 
+    #   (i.e. no example data and data correctly loaded)
+    req(input$records_input)
     
-    # Placeholder to allow to select no column
-    nullval <- "Not present in data"
-    validate(need(!(nullval %in% records_col),
+    validate(need(!(nullval %in% records_cols()),
                   paste("Please provide a dataframe with no column named",
                         nullval)))
     
     # Find default names
-    default_names <- find_default_colnames(widget_list,
-                                           records_col,
+    default_names <- find_default_colnames(records_cols_wanted(),
+                                           records_cols(),
                                            empty_allowed,
                                            empty_placeholder = nullval)
-    
-    for(w in widget_list) {
+    default_names
+  })
+  
+  # Update selection list and default names 
+  # in selectInput for records
+  observeEvent(input$records_input, {
+    for(w in records_cols_wanted()) {
       # Get default
-      default_name <- default_names[[w]]
+      default_name <- default_records()[[w]]
         
       if(w %in% empty_allowed) {
         updateSelectInput(session = session,
                           inputId = w,
-                          choices = c(nullval, records_col),
+                          choices = c(nullval, records_cols()),
                           selected = default_name)
       } else {
         updateSelectInput(session = session,
                           inputId = w,
-                          choices = records_col,
+                          choices = records_cols(),
                           selected = default_name)
       }
-      
     }
+  })
+  
+  # Mapping value for records columns
+  mapping_records <- reactive({
+    # Get relevant widgets
+    widgets <- records_cols_wanted()
     
+    # Get selected values
+    res <- vector(mode = "charactger", 
+                  length = length(widgets))
+    for(i in 1:length(widgets)) {
+      res[i] <- input[[widgets[i]]]
+    }
+    names(res) <- widgets
+    
+    cat(res[1])
+    res
   })
 
 # File input preview ------------------------------------------------------
