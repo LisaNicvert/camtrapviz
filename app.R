6ad10bfacd66244992137f8e669afe1c96a8e60f
Lisa Nicvert <lisa.nicvert@univ-lyon1.fr>
2024-04-19 13:16:49 +0200

    Added resource path to launch script

--- app.R
+++ app.R
@@ -1,4 +1,6 @@
 # Launch the ShinyApp (Do not remove this comment)
 # for the LBBE shiny server
 
+shiny::addResourcePath('www', system.file("www", package = "camtrapviz"))
+
 camtrapviz::run_camtrapviz()
